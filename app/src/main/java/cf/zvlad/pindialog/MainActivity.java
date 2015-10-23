36873c1df074269e87be6b2392bfd322d54f5a5b
Vlad Zamskoi <v.zamskoi@gmail.com>
2015-10-23 20:52:00 +0300

    Primary code cleaning is done.

--- app/src/main/java/cf/zvlad/pindialog/MainActivity.java
+++ app/src/main/java/cf/zvlad/pindialog/MainActivity.java
@@ -1,7 +1,11 @@
 package cf.zvlad.pindialog;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import cf.zvlad.pindialoglibrary.PinDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hello!", Toast.LENGTH_LONG).show();
+        Dialog d = new PinDialog(this, 2);
+        d.show();
     }
 }
